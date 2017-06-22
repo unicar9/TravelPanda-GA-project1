9a03902e42401f32e6fd9d87eb19b6f75073da44
Lingxiao Wang <Lingxiao@MacBook-Pro.local>
2017-06-22 11:43:05 +1000

    ah what am i going to do

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,17 +1,24 @@
 class DaysController < ApplicationController
   before_action :get_day, only: [:show, :edit, :update, :destroy]
 
+  before_action :get_trip
+
   def get_day
     @day = Day.find params["id"]
   end
 
-  def new
-    @day = Day.new
+  def get_trip
     @trip = Trip.find params["trip_id"]
   end
 
-  def create
+  def new
+    @day = Day.new
+  end
 
+  def create
+    @day = Day.create day_params
+    @trip.days << @day
+    redirect_to trip_day_path(@trip,@day)
   end
 
   def index
@@ -29,11 +36,12 @@ class DaysController < ApplicationController
   end
 
   def destroy
-
+    @day.destroy
+    redirect_to trip_path(@trip)
   end
 
   private
   def day_params
-    params.require(:day).permit(:trip_date, :trip_id)
+    params.require(:day).permit(:trip_date)
   end
 end
